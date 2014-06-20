e5adadfed646e9b002592ec40406e06b2aea11f8
k33711647 <mail@kevin-beck.co.uk>
2014-06-20 14:12:39 +0100

    initial commit

---
+++ Readme.md
@@ -0,0 +1,26 @@
+Parallax with Skrollr
+====================
+Version 0.0.1
+
+A ultra simple one page starter template for experiments with parallax using Skroller.js
+
+Team
+-------------
+
+Devlopment: Kevin Beck (kevin@multiplestates.co.uk)
+
+Attributions
+-------------
+
+Thanks to Prinzhorn for Skrollr.js
+
+https://github.com/Prinzhorn/skrollr
+
+-
+
+For adding animation keyframes in your stylesheet we used skrillr-stylesheets by Prinzhorn
+
+https://github.com/Prinzhorn/skrollr-stylesheets 
+
+
+
---
+++ index.php
@@ -0,0 +1,43 @@
+ <!DOCTYPE html> 
+
+<html lang="en">
+  
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Test tube</title>
+    
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <?php include('functions.php') ?>
+    
+    <link rel="stylesheet" href="<?php echo $site_url ?>src/css/normalize.css" >
+    <link rel="stylesheet" href="<?php echo $site_url ?>style.css" data-skrollr-stylesheet >
+  </head>
+  
+  <!-- add id for mobile support - https://github.com/Prinzhorn/skrollr#mobile-support -->
+  <body id="skrollr-body">
+    
+    <h1 id="fx-title">this is a title that will change size</h1>
+    
+    <!-- External scripts -->
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+    <!-- skrollr stylesheet support: https://github.com/Prinzhorn/skrollr-stylesheets -->
+    <script src="<?php echo $site_url ?>skrollr.stylesheets.js"></script>
+
+    <!-- skrollr paralex FX: https://github.com/Prinzhorn/skrollr -->
+    <script src="<?php echo $site_url ?>skrollr.js"></script>
+
+    <!-- set up skroller -->
+    <script type="text/javascript">
+        skrollr.init();
+    </script>
+
+    <script src="<?php echo $site_url ?>custom.js"></script>
+  
+  </body>
+  
+</html>
